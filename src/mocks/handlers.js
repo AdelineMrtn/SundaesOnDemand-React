1916d7224d3445748827c3a0df637a877a68a5ea
AdelineMrtn <adeline.martineau0@gmail.com>
2022-03-15 17:33:38 +0100

    Code and test options images from server

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,4 +1,5 @@
 import { rest } from 'msw';
+
 export const handlers = [
     rest.get("http://localhost:3030/scoops", (req, res, ctx) => {
         return res(
---
+++ src/pages/entry/Options.jsx
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';   
+import ScoopOptions from './ScoopOption';
+import Row from 'react-bootstrap/Row';  
+
+
+export default function Options({ optionType }) {
+    const [items, setItems] = useState([]);
+
+    // option type is 'scoops' or 'toppings'   
+    useEffect(() => {
+        axios
+        .get(`http://localhost:3030/${optionType}`)
+        .then((response) => setItems(response.data))
+        .catch(error => {
+            //TODO: do later
+        });
+    }, [optionType]);
+
+    const ItemsComponents = optionType === 'scoops' ? ScoopOptions : null;
+    const optionItems = items.map((item) => (
+        <ItemsComponents 
+            key={item.name} 
+            name={item.name} 
+            imagePath={item.imagePath}
+        />
+    )); 
+    return <Row>{optionItems}</Row>;
+}
+
